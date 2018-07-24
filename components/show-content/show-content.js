a2fff1f33151f2ea0881de8386805503cdf6d216
SpaceWarUnicorn <theeternalpuzzle@gmail.com>
2018-07-24 17:58:36 -0500

    feat: finished // web components communicated

--- components/show-content/show-content.js
+++ components/show-content/show-content.js
@@ -1,11 +1,16 @@
 
 
 (function showContentDefiniton(newElement){
-   
+
 'use strict';
 
+
+
+
+
+
 class ShowContent extends HTMLElement{
- 
+
   constructor(){
     super();
 
@@ -16,23 +21,97 @@ class ShowContent extends HTMLElement{
 
    let shadowRoot = this.attachShadow({mode: 'open'});
    const templateImport = document.querySelector('link[href="components/show-content/show-content.html"]').import;
-   
-   //const templateImport = documentImport.querySelector('link[rel="import"]').import;
-console.log(templateImport)
 
    const template = templateImport.querySelector('#show-content');
    const instance = template.content.cloneNode(true);
    shadowRoot.appendChild(instance);
-  this.addEventListener('navigation-active', (event)=>{
-    console.log("Listener show content", event)
-  });
-   
+    document.addEventListener('navigation-active', (data)=>{
+      let superhero = data.detail.hero;
+      let card = '';
+      if(!superhero == "" )
+      {
+
+       card = `
+        <section class="container card">
+            <article class="items" id="${superhero._id}">
+              <label class="item texto name">
+                ${superhero.hero}
+              </label><label class="item texto name">
+                ${superhero.universo}
+              </label><label class="item texto name">
+                ${superhero.identidad}
+              </label>
+              <button class="hero ${superhero._id}">Eliminar</button>
+          </article>
+        </section>`;
+      }
+      let content = shadowRoot.querySelector('#container');
+      content.innerHTML += card;
+    });
+
+    this.shadowRoot.addEventListener('click', (e) => {
+      let element = e.target.getAttribute('class');
+      let btn = [];
+      let id = '';
+      let label= '';
+      if (element != null){
+        btn = element.split(' ');
+      }
+      if (btn[0] == 'hero'){
+        id = element.split(' ')[1];
+
+        label = this.shadowRoot.querySelector('#'+id);
+        console.log(id);
+        label.innerHTML='';
+      }
+
+
+
+      this.dispatchEvent(new CustomEvent('killEvt', {
+        bubbles:true,
+        composed:true,
+        detail:{id}
+      }));
+    });
+
   }
-}
+
+  /*add(data) {
+    let newCard = this.createCard(data);
+    let content = this.shadowRoot.querySelector('#container');
+    content.innerHTML += newCard;
+
+    const butt = this.shadowRoot.querySelector('#hero'+data.hero._id);
+     butt.addEventListener('click', () => {
+       alert('workea');
+     });
+  }
+
+  createCard(data) {
+    let superhero = data.hero;
+    let card = '';
+    if(!superhero == "" )
+    {
+
+     card = `
+      <section class="container card">
+          <article class="items">
+            <label class="item texto name">
+              ${superhero.hero}
+            </label><label class="item texto name">
+              ${superhero.universo}
+            </label><label class="item texto name">
+              ${superhero.identidad}
+            </label>
+            <button id="hero${superhero._id}">Eliminar</button>
+        </article>
+      </section>`;
+    }
+    return card;
+  }*/
+
+  }
 
 newElement.define('show-content', ShowContent);
 
-})(window.customElements);  
-
-
-
+})(window.customElements);
--- components/sidebar-navigation/sidebar-navigation.js
+++ components/sidebar-navigation/sidebar-navigation.js
@@ -1,11 +1,11 @@
 
 
 (function sidebarNavigationDefiniton(newElement){
-   
+
 'use strict';
 
 class SidebarNavigation extends HTMLElement{
- 
+
   constructor(){
     super();
 
@@ -13,28 +13,72 @@ class SidebarNavigation extends HTMLElement{
 
   connectedCallback () {
 
+    let superhero = [
+      {
+        _id:'one',
+        hero: 'Spiderman',
+        universo:'marvel',
+        identidad:'peter parker'
+      },
+      {
+        _id:'two',
+        hero: 'Batman',
+        universo:'DC',
+        identidad:'Bruno Diaz'
+      },
+      {
+        _id:'three',
+        hero: 'Goku',
+        universo:'Dragon Ball',
+        identidad:'Son-Goku'
+      }
+    ]
+
    let shadowRoot = this.attachShadow({mode: 'open'});
    const templateImport = document.querySelector('link[rel="import"]').import;
    const template = templateImport.querySelector('#sidebar-navigation');
    const instance = template.content.cloneNode(true);
    shadowRoot.appendChild(instance);
 
-   this.addEventListener('click', (event)=>{
-     const element = event.target;
+
+   this.shadowRoot.addEventListener('click', (event)=>{
+     let element = event.target.innerHTML;
+     let hero='';
+     for (let i in superhero){
+       if (element == superhero[i].hero){
+         hero = superhero[i];
+       }
+     }
+
       this.dispatchEvent(new CustomEvent('navigation-active', {
         bubbles:true,
-        composed:false,
-        detail:{element}
+        composed:true,
+        detail:{hero}
       }));
    });
-   
+
+     document.addEventListener('killEvt', (data)=>{
+       let id = '';
+       let hero='';
+       let label = ''
+       if(data.detail.id!= ''){
+         id = data.detail.id;
+       }
+
+       for (let i in superhero){
+         if (id == superhero[i]._id){
+           hero = superhero[i].hero.toLowerCase();
+           label = this.shadowRoot.querySelector('#'+hero)
+           label.innerHTML='';
+         }
+       }
+
+
+
+     });
   }
 }
 
 newElement.define('sidebar-navigation', SidebarNavigation);
 
-})(window.customElements);  
-
-
-
-
+})(window.customElements);
